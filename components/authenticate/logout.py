dbc2463a16a7182e1fe4e1e7499f8d6df79bdc51
CTT623-Linux-1752047 <40145559+CTT623-Linux-1752047@users.noreply.github.com>
2022-04-11 17:44:28 +0700

    update layout index
    update layout index

--- components/authenticate/logout.py
+++ components/authenticate/logout.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 
 
 def signout(request):
